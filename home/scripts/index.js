ee5e12c94e1e3404b8f0558473d9c81923f3543c
montali <100239487+mnlyx@users.noreply.github.com>
2023-06-29 18:55:57 +0000

    UI 2.0

--- home/scripts/index.js
+++
@@ -1,9 +0,0 @@
-// Evento que é acionado quando a página é carregada
-window.addEventListener('load', function() {
-    // Selecione o elemento raiz (html)
-    var rootElement = document.documentElement;
-  
-    // Role a página até o elemento raiz (topo da página)
-    rootElement.scrollIntoView({ behavior: 'smooth', block: 'start' });
-  });
-  
